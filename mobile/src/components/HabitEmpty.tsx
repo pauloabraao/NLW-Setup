8c9840962307b268a9dca9c4373c572be368b8a2
pauloabraao <abraaoteles@alu.ufc.br>
2023-01-21 17:49:16 -0300

    finished NLW-Setup

---
+++ mobile/src/components/HabitEmpty.tsx
@@ -0,0 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
+import { Text } from "react-native";
+
+export function HabitsEmpty() {
+  const { navigate } = useNavigation();
+  return (
+    <Text className="text-zinc-400 text-base ">
+      You are not tracking any habits yet{" "}
+      <Text
+        className="text-violet-400 text-base underline active:text-violet-500"
+        onPress={() => navigate("new")}
+      >
+        create the first one here!
+      </Text>
+    </Text>
+  );
+}
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,5 +1,5 @@
-import { useNavigation } from "@react-navigation/native";
-import { useState, useEffect } from "react";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { useState, useEffect, useCallback } from "react";
 import { View, Text, ScrollView, Alert } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -39,9 +39,11 @@ export function Home() {
     }
   }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  useFocusEffect(
+    useCallback(() => {
+      fetchData();
+    }, [])
+  );
 
   if (loading) {
     return <Loading />;
